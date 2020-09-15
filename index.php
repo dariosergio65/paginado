ddd7d4f8d22ee1bfd07ee75ecc92cde2cb68770e
dariosergio65 <dariosergio@hotmail.com>
2020-09-14 22:24:09 -0300

    Paginado 3 de Pildoras

--- index.php
+++ index.php
@@ -11,19 +11,26 @@ $cuantos = 10; //cuantos registros muestro por pagina
 $pag_actual = 1; //numero de pagina actual
 $paginas = 1; //cantidad de paginas para mostrar
 
+
+
     $sql= "SELECT Cliente FROM clientes";
     $resultado = $base->prepare($sql);
     $resultado->execute(array());
 
     $num_filas= $resultado->rowCount(); //cantidad de registros q devuelve la consulta
 
-    if ($num_filas > 10 ){
+    if ($num_filas > $cuantos ){
         $paginas = ceil ($num_filas/$cuantos); 
     }
 
+    if (isset($_GET['pag'])){
+        $pag_actual = $_GET['pag'];
+        $inicio = ($pag_actual - 1) * $cuantos;
+    }
+
     echo "Número de filas: " . $num_filas . "<br>";
-    echo "Registros por página: " . $cuantos . "<br>";
-    echo "Mostrando página " . $pag_actual . " de " . $paginas . "<br>";
+    echo "Registros por página: " . $cuantos . "<br><br>";
+    
 
     $sql= "SELECT Cliente FROM clientes LIMIT $inicio, $cuantos";
     $resultado = $base->prepare($sql);
@@ -37,9 +44,39 @@ while ($registro=$resultado->fetch(PDO::FETCH_ASSOC)){
 
     $resultado->closeCursor();
 
-//echo "hasta aca funciona";
+//-----------------paginado-------------------------
+//echo "<br><br>";
+
+//for ($i=1; $i<=$paginas; $i++ ){
+    
+//    echo " <a href='?pag=" . $i . "'>" . $i . "</a> ";
+//}
+echo "<br><br>";
+echo "Página " . $pag_actual . " de " . $paginas . "<br><br>";
 
 ?>
+
+<nav aria-label="Page navigation example">
+    <ul class="pagination">
+        <?php for ($i=$pag_actual; $i<=$paginas; $i++ ){
+                if ($i==$pag_actual){
+                    if($i>$cuantos){
+                        $prev=$i-$cuantos;
+                    }else {
+                        $prev=1;
+                    }
+                    echo '<li class="page-item"><a class="page-link" href="?pag=' . $prev . '">PREVIA</a></li>';
+                }elseif ($i==$pag_actual+$cuantos) {
+                    $prox=$i;
+                    $i=$paginas+1;
+                    echo '<li class="page-item"><a class="page-link" href="?pag=' . $prox . '">PROXIMA</a></li>';
+                }else {
+                    echo '<li class="page-item"><a class="page-link" href="?pag=' . $i . '">' . $i . '</a></li>' ;
+                }
+            }
+        ?>
+    </ul>
+</nav>
 
 <?php
 include ("includes/footer.php");
